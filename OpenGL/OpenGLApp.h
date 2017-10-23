5210e485b97d5a395ca990fc6b32998addbc1a98
Alex Lup <alexlup@ymail.com>
2017-10-23 09:01:11 +0100

    OpenGL base app class start.

--- OpenGL/OpenGLApp.h
+++ OpenGL/OpenGLApp.h
@@ -1,20 +1,41 @@
 #ifndef OPENGLAPP_H
 #define OPENGLAPP_H
 
+// ----------------------------------------------------------------------------
+
+#include "Common.h"
+
+// ----------------------------------------------------------------------------
+
 class OpenGLApp
 {
 public:
-	OpenGLApp();
+	OpenGLApp(int windowWidth, int windowHeight);
 	~OpenGLApp();
 
-	void Run();
+	void run();
+
+	// Getters/Setters
+	inline const GLFWwindow* window() const { return m_pWindow; }
 
 protected:
-	virtual void Update(float dt);
-	virtual void Draw(float dt);
+	virtual void update(float dt);
+	virtual void draw(float dt);
 
-	virtual int Initialize();
-	virtual void SetupScene();
+	virtual bool initialize(const char* windowTitle, bool enableMultisampling, bool enableSRGB);
+	virtual void setupScene();
+
+private:
+	GLFWwindow* m_pWindow;
+
+	// Window size
+	int m_iWindowWidth;
+	int m_iWindowHeight;
+
+	// Set OpenGL state
+	bool setOpenGLState() const;
 };
+
+// ----------------------------------------------------------------------------
 
 #endif // OPENGLAPP_H
